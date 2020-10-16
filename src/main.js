10988145592295360fb7aeb689df496230ced0a2
MaxPowerEdain <56118011+MaxPowerEdain@users.noreply.github.com>
2020-10-16 11:42:28 +0200

    Refactorizadas las funciones de añadir y borrar coders

--- src/main.js
+++ src/main.js
@@ -1,19 +1,39 @@
 'use strict'
 
-document.getElementById("btnAddCoder").addEventListener('click', addCoder);
-const codersDisplay = document.querySelector(".coders");
+const btnAddCoder = document.getElementById("btnAddCoder");
+const codersDisplay = document.getElementById("coders");
+const coderNameInput = document.getElementById("coderName");
 
 function addCoder() {
-    let nombreCoder = "MaxPower";
-    codersDisplay.innerHTML += `
-    <div class="coder">
-        <img src="img/${nombreCoder}.jpg" alt="Imagen de ${nombreCoder}" style="height: 80px;">
-        <button class="btnDeleteCoder">X</button>
-    </div>`;
+    let coderNameValue = getCoderNameInput();
+    codersDisplay.innerHTML += createCoderCard(coderNameValue);
+    clearCoderNameInput();
 };
 
-codersDisplay.addEventListener('click', function(e){
-    if (e.target && e.target.matches(".btnDeleteCoder")) {
-        e.target.parentNode.remove();
+function clearCoderNameInput(){
+    coderNameInput.value = "";
+};
+
+function createCoderCard(coderName){
+    return `
+        <div class="coder">
+            <img src="img/${coderName}.jpg" alt="Imagen de ${coderName}"">
+            <button class="btnDeleteCoder">X</button>
+        </div>`;
+};
+
+function deleteCoderCard(event){
+    if (event.target && event.target.matches(".btnDeleteCoder")) {
+        event.target.parentNode.remove();
     }
+};
+
+function getCoderNameInput(){
+    return coderNameInput.value;
+};
+
+btnAddCoder.addEventListener('click', addCoder);
+
+codersDisplay.addEventListener('click', function(event){
+    deleteCoderCard(event);
 });
